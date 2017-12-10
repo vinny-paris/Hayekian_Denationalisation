3e51f3aab621db85a4b64494567c9f09560476f8
vinny-paris <vinny@iastate.edu>
2017-12-09 18:34:30 -0600

    expansions

--- part1_code.R
+++ part1_code.R
@@ -1,4 +1,5 @@
 setwd('/Users/vincentparis/Counts')
+
 danish <- read.csv('danish.csv', header = T)
 names(danish) <- c('alc', 'income', 'mar', 'urban', 'count')
 head(danish)
@@ -31,7 +32,7 @@ goftests(danish$count, ind_mod$fitted.values, danish$count, ind_mod$df.residual)
 
 
 
-
+library(MASS)
 
 bot <- stepAIC(ind_mod, scope = list(lower = formula(ind_mod), upper =~ .^4))
 
@@ -41,7 +42,7 @@ nearex_mod <- glm(count ~ alc*income*urban*mar - alc:income:urban:mar, family=po
 
 library(MASS)
 final_mod <- stepAIC(nearex_mod, scope = list(upper = nearex_mod, lower = ~1))
-final_mod[[length(fin)]]
+
 
 final_mod <- glm(count ~ alc + income + urban + mar + alc:income + alc:urban + 
     income:urban + alc:mar + income:mar + urban:mar + alc:income:mar, family = poisson,data = danish, maxit=20, epsilon=.000001, x=T, trace=T)
@@ -69,27 +70,35 @@ goftests(danish$count, nearex_mod$fitted.values, danish$count, nearex_mod$df.res
 
 
 #Overdispersion Checking
-neamod <- glm(count ~ alc*income*urban*mar - alc:income:urban:mar, family=quasipoisson,
-              data = danish, maxit=20, epsilon=.000001, x=T, trace=T)
-
-fine <- stepAIC(nearex_mod, scope = list(upper = nearex_mod, lower = ~1))
 fine <- glm(formula = count ~ alc + income + urban + mar + alc:income + 
     alc:urban + income:urban + alc:mar + income:mar + urban:mar + 
-    alc:income:mar, family = poisson, data = danish, x = T, maxit = 20, 
-    epsilon = 1e-06, trace = T, Hessian = T)
+    alc:income:mar, family = quasipoisson, data = danish, x = T, maxit = 20, 
+    epsilon = 1e-06, trace = T)
+summary(fine)
 anova(fine, test = 'Chisq')
-vc <- vcov(fine)
-
-para <- fine$coefficients
-se <- summary(fine)[[12]][,2]
 
 
-con <- confint(fine)
+
+
+#The ending model, using quasipoisson with an overdispersion factor of 1.57701
+end <- glm(formula = count ~ alc + income + urban + mar + alc:income + 
+              alc:urban + income:urban + alc:mar + income:mar + urban:mar, family = quasipoisson, data = danish, x = T, maxit = 20, 
+            epsilon = 1e-06, trace = T)
+anova(end, test = 'Chisq')
+
+vc <- vcov(end)
+
+para <- end$coefficients
+se <- summary(end)[[12]][,2]
+
+
+con <- confint(end)
 con.int <- exp(con)
 holding <- data.frame(para, se, con.int)
 names(holding) <- c("Parameter", 'Std. Error', "Lower CI", "Upper CI")
 
 
+library(ggplot2)
 ggplot(danish, aes(urban, count)) + geom_point(aes(col = mar)) + facet_grid(~income) + ggtitle('Income')
 ggplot(danish, aes(mar, count)) + geom_point(aes(col = alc)) + facet_grid(~income) + ggtitle('Income')
 
@@ -97,27 +106,28 @@ ggplot(danish, aes(mar, count)) + geom_point(aes(col = alc)) + facet_grid(~incom
 
 
 #log_it_1vs2 
-data.frame(1:68, holding)
-al2 <- c(2, 13,15,17,19,21, 23,25,39, 41, 57,59,61,63,65,67)
+data.frame(1:56, holding)
+al2 <- c(2, 13,15,17,19,21, 23,25,39, 41)
 al3 <- al2+1 
 log_it_1vs2 <- holding[al2,]
 k <- rownames(log_it_1vs2)
-rownames(log_it_1vs2) <- unlist(strsplit(k, 'alc2.'))[seq(1,31,2)]
+rownames(log_it_1vs2) <- unlist(strsplit(k, 'alc2.'))[seq(1,20,2)]
 rownames(log_it_1vs2)[1] <- 'int'
 log_it_1vs2
 
 
 log_it_1vs3 <- holding[al3,]
 k <- rownames(log_it_1vs3)
-rownames(log_it_1vs3) <- unlist(strsplit(k, 'alc3.'))[seq(1,31,2)]
+rownames(log_it_1vs3) <- unlist(strsplit(k, 'alc3.'))[seq(1,20,2)]
+rownames(log_it_1vs3)[1] <- 'int'
 log_it_1vs3 
 
 
-
-
-
-
-
+library(GoodmanKruskal)
+library(reshape2)
+urban_income <- acast(danish, income ~ urban, sum)
+urban_income
+GKtauDataframe(urban_income)
 
 
 
